a698a06e0bc26bbd57ab7d77e7eeac7e3872c8d2
Sijun <sionwer4@naver.com>
2022-11-09 18:22:33 +0900

    feat: fist commit

---
+++ src/server.js
@@ -0,0 +1,61 @@
+import http from "http";
+// import WebSocket from "ws";
+import express from "express";
+import SocketIO from "socket.io"
+
+const app = express();
+
+app.set('view engine', "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", socket => {
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) =>{
+        socket.to(roomName).emit("ice", ice);
+    })
+});
+
+
+/* 
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
+    console.log("Connected to Broswer");
+    socket.on("close", () => {
+        console.log("Disconnected from Browser")
+    });
+    socket.on("message", (message) => {
+        const parsed = JSON.parse(message);
+        switch(parsed.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}:${parsed.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = parsed.payload;
+                console.log("Done");
+                break;
+        }
+    });
+}); */
+
+httpServer.listen(3000, handleListen);
